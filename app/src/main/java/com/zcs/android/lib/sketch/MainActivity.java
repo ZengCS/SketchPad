baaca4bc725527d390ff0f92330b3f62fea3b970
chengshun.zeng <zcs@sxw.cn>
2018-12-26 12:28:07 +0800

    create module

---
+++ app/src/main/java/com/zcs/android/lib/sketch/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zcs.android.lib.sketch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
